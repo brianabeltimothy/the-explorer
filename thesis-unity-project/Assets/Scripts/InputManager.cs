65fbf6c7ee6c158497f8256e98bff76d52b2f1b6
brianabeltimothy <brianabeltimothyd@gmail.com>
2024-02-20 18:38:18 +0000

    player controller - jump, fix animations, files management

--- thesis-unity-project/Assets/Scripts/InputManager.cs
+++ thesis-unity-project/Assets/Scripts/InputManager.cs
@@ -9,25 +9,30 @@ public class InputManager : MonoBehaviour
     public Vector2 Move {get; private set;}
     public Vector2 Look {get; private set;}
     public bool Run {get; private set;}
+    public bool Jump {get; private set;}
 
     private InputActionMap currentMap;
     private InputAction moveAction;
     private InputAction lookAction;
     private InputAction runAction;
+    private InputAction jumpAction;
 
     private void Start() {
         currentMap = playerInput.currentActionMap;
         moveAction = currentMap.FindAction("Move");
         lookAction = currentMap.FindAction("Look");
         runAction = currentMap.FindAction("Run");
+        jumpAction = currentMap.FindAction("Jump");
 
         moveAction.performed += onMove;
         lookAction.performed += onLook;
         runAction.performed += onRun;
+        jumpAction.performed += onJump;
 
         moveAction.canceled += onMove;
         lookAction.canceled += onLook;
         runAction.canceled += onRun;
+        jumpAction.canceled += onJump;
     }
 
     private void onMove(InputAction.CallbackContext context)
@@ -41,6 +46,10 @@ public class InputManager : MonoBehaviour
     private void onRun(InputAction.CallbackContext context)
     {
         Run = context.ReadValueAsButton();
+    }
+    private void onJump(InputAction.CallbackContext context)
+    {
+        Jump = context.ReadValueAsButton();
     }
 
     private void OnEnable() {
--- thesis-unity-project/Assets/Scripts/PlayerController.cs
+++ thesis-unity-project/Assets/Scripts/PlayerController.cs
@@ -1,39 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Interactions;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float animationBlendSpeed = 8.9f;
+    [SerializeField] private float animationBlendSpeed = 9f;
     [SerializeField] private Transform cameraRoot;
     [SerializeField] private Transform cam;
     [SerializeField] private float upperLimit = -40f;
     [SerializeField] private float bottomLimit = 70f;
-    [SerializeField] private float mouseSensitivity = 21f;
+    [SerializeField] private float mouseSensitivity = 5;
+    [SerializeField] private bool heabob = true;
+    [SerializeField] private float jumpForce = 1000f;
+    [SerializeField] private float distanceToGround = 0.8f;
+    [SerializeField] private LayerMask groundCheck;
+    [SerializeField] private float airResistance = 0.8f;
     private Rigidbody playerRb;
     private InputManager inputManager;
     private Animator animator;
     private bool hasAnimator;
+    private bool grounded;
     private int xVelHash;
     private int yVelHash;
+    private int zVelHash;
+    private int jumpHash;
+    private int fallingHash;
+    private int groundedHash;
     private float xRotation; //cam rotation in x axis
-    private const float walkSpeedAnim = 1.5f;
-    private const float runSpeedAnim = 3f;
+    private const float walkSpeedAnim = 2f;
+    private const float runSpeedAnim = 6f;
     private Vector2 currentVelocity;
 
-    private void Awake() {
+    private void Awake() 
+    {
         playerRb = GetComponent<Rigidbody>();
         inputManager = GetComponent<InputManager>();
         hasAnimator = TryGetComponent<Animator>(out animator);
 
         xVelHash = Animator.StringToHash("X_Velocity");
         yVelHash = Animator.StringToHash("Y_Velocity");
+        zVelHash = Animator.StringToHash("Z_Velocity");
+        jumpHash = Animator.StringToHash("Jump");
+        fallingHash = Animator.StringToHash("Falling");
+        groundedHash = Animator.StringToHash("Grounded");
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void FixedUpdate()
     {
+        SampleGround();
         Move();
+        HandleJump();
+    }
+
+    private void LateUpdate() 
+    {
         CameraMovement();
     }
 
@@ -44,11 +66,20 @@ public class PlayerController : MonoBehaviour
         float targetSpeed = inputManager.Run ? runSpeedAnim : walkSpeedAnim;
         if(inputManager.Move == Vector2.zero) targetSpeed = .1f;
 
-        currentVelocity.x = Mathf.Lerp(currentVelocity.x, targetSpeed * inputManager.Move.x, animationBlendSpeed * Time.deltaTime);
-        currentVelocity.y = Mathf.Lerp(currentVelocity.y, targetSpeed * inputManager.Move.y, animationBlendSpeed * Time.deltaTime);
-  
-        playerRb.AddForce(transform.right * targetSpeed * inputManager.Move.x, ForceMode.VelocityChange);
-        playerRb.AddForce(transform.forward * targetSpeed * inputManager.Move.y, ForceMode.VelocityChange);
+        if (grounded)
+        {
+            Vector3 targetVelocity = new Vector3(targetSpeed * inputManager.Move.x, 0, targetSpeed * inputManager.Move.y);
+
+            currentVelocity.x = Mathf.Lerp(currentVelocity.x, targetVelocity.x, animationBlendSpeed * Time.deltaTime);
+            currentVelocity.y = Mathf.Lerp(currentVelocity.y, targetVelocity.z, animationBlendSpeed * Time.deltaTime);
+
+            playerRb.AddForce(transform.right * targetSpeed * inputManager.Move.x, ForceMode.VelocityChange);
+            playerRb.AddForce(transform.forward * targetSpeed * inputManager.Move.y, ForceMode.VelocityChange);
+        }
+        else
+        {
+            playerRb.AddForce(transform.TransformVector(new Vector3(currentVelocity.x * airResistance, 0, currentVelocity.y * airResistance)), ForceMode.VelocityChange);
+        }
 
         animator.SetFloat(xVelHash, currentVelocity.x);
         animator.SetFloat(yVelHash, currentVelocity.y);
@@ -58,16 +89,57 @@ public class PlayerController : MonoBehaviour
     {
         if(!hasAnimator) return;
 
-        float mouseX = inputManager.Look.x * mouseSensitivity * Time.deltaTime;
-        float mouseY = inputManager.Look.y * mouseSensitivity * Time.deltaTime;
+        float mouseX = inputManager.Look.x * mouseSensitivity * Time.smoothDeltaTime;
+        float mouseY = inputManager.Look.y * mouseSensitivity * Time.smoothDeltaTime;
         
         //for headbob can be disable or not
-        cam.position = cameraRoot.position;
+        if(heabob) cam.position = cameraRoot.position;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, upperLimit, bottomLimit);
 
         cam.localRotation  = Quaternion.Euler(xRotation, 0f, 0f);
-        this.transform.Rotate(Vector3.up, mouseX * mouseSensitivity * Time.deltaTime);
+        playerRb.MoveRotation(playerRb.rotation * Quaternion.Euler(0f, mouseX, 0f));
+        this.transform.Rotate(Vector3.up, mouseX);
+    }
+
+    private void HandleJump()
+    {
+        if(!hasAnimator) return;
+        if(!inputManager.Jump) return;
+        if(!grounded) return;
+        animator.SetTrigger(jumpHash);
+    }
+
+    private void JumpAddForce()
+    {
+        playerRb.AddForce(playerRb.velocity.y * Vector3.up, ForceMode.VelocityChange);
+        playerRb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+        animator.ResetTrigger(jumpHash);
+    }
+
+    private void SampleGround()
+    {
+        if(!hasAnimator) return;
+
+        RaycastHit hitInfo;
+        if(Physics.Raycast(playerRb.worldCenterOfMass, Vector3.down, out hitInfo, distanceToGround + 0.1f, groundCheck))
+        {
+            grounded = true;
+            Debug.DrawRay(transform.position, hitInfo.point, Color.green);
+            SetAnimationGrounding();
+            return;
+        }
+        // Raycast did not hit anything or falling
+        grounded = false;
+        animator.SetFloat(zVelHash, playerRb.velocity.y);
+        SetAnimationGrounding();
+        return;
+    }
+
+    private void SetAnimationGrounding()
+    {
+        animator.SetBool(fallingHash, !grounded);
+        animator.SetBool(groundedHash, grounded);
     }
 }
