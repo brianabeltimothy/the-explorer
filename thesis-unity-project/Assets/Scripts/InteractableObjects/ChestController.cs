89b4b67c1e32feeb60379b1ae8f5f01be9c9001b
brianabeltimothy <brianabeltimothyd@gmail.com>
2024-07-26 15:39:30 +0200

    fix chest bug

--- thesis-unity-project/Assets/Scripts/InteractableObjects/ChestController.cs
+++ thesis-unity-project/Assets/Scripts/InteractableObjects/ChestController.cs
@@ -7,18 +7,11 @@ public class ChestController : MonoBehaviour, IInteractable
     private Animator animator;
     private BoxCollider boxCollider;
     private AudioSource audioSource;
-    [SerializeField] private GameObject chestLid;
-    private Vector3 targetRotation = new Vector3(100, 0, 0); 
 
     private void Awake() {
         animator = GetComponent<Animator>();
         boxCollider = GetComponent<BoxCollider>();
         audioSource = GetComponent<AudioSource>();
-    }
-
-    private void Start() {
-        Transform secondChildTransform = transform.GetChild(1);
-        chestLid = secondChildTransform.gameObject;
     }
 
     public void Interact()
@@ -28,26 +21,12 @@ public class ChestController : MonoBehaviour, IInteractable
 
     private void OpenChest()
     {
-        // animator.SetTrigger("Open");
-        StartCoroutine(OpenChestCoroutine());
+        animator.SetTrigger("Open");
         boxCollider.enabled = false;
-        audioSource.Play();
     }
 
-    IEnumerator OpenChestCoroutine()
+    private void PlaySound()
     {
-        Quaternion initialRotation = chestLid.transform.rotation;
-        Quaternion finalRotation = Quaternion.Euler(targetRotation);
-        float elapsedTime = 0f;
-        float duration = 0.85f;
-
-        while (elapsedTime < duration)
-        {
-            chestLid.transform.rotation = Quaternion.Slerp(initialRotation, finalRotation, elapsedTime / duration);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        chestLid.transform.rotation = finalRotation;
+        audioSource.Play();
     }
 }
